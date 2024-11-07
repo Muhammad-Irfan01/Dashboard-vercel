c3ce99d83029c3343a4744c2af80ec0d44bbf924
Muhammad-Irfan01 <irfan.bvoir@gmail.com>
2024-11-07 12:31:00 +0500

    Update vite.config.js
    iiiiii

--- shopify-dashboard-art-gallery/vite.config.js
+++ shopify-dashboard-art-gallery/vite.config.js
@@ -1,22 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-     alias: {
-      '@': path.resolve(__dirname, 'src'),
-      'ArtGallery': path.resolve(__dirname, 'src/Art-Gallery'),
-      'Dashboard': path.resolve(__dirname, 'src/src-dashboard')
-    }
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'src/Art-Gallery/index.html'),
-        admin: path.resolve(__dirname, 'src/src-dashboard/index.html')
-      }
-    }
-  }
 })
