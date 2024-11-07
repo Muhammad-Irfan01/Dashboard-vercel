09b1635e394c2c0984c7c7079692a68a15573365
Muhammad-Irfan01 <irfan.bvoir@gmail.com>
2024-11-07 10:46:55 +0500

    Update vite.config.js
    updated

--- shopify-dashboard-art-gallery/vite.config.js
+++ shopify-dashboard-art-gallery/vite.config.js
@@ -3,4 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+      'ArtGallery': '/src/Art-Gallery',
+      'Dashboard': '/src/src-dashboard'
+    }
+  },
 })
