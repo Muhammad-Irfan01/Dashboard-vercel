8c44f0bb18ef14851e439980fafeb35d97165eb0
Muhammad-Irfan01 <irfan.bvoir@gmail.com>
2024-11-08 11:53:44 +0500

    Update main.tsx
    stylijgn

--- shopify-dashboard-art-gallery/src/src-dashboard/main.tsx
+++ shopify-dashboard-art-gallery/src/src-dashboard/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import AdminApp from './AdminApp.tsx';
 import { BrowserRouter } from 'react-router-dom';
+import '../src-dashboard/Pages/Dashboard.css';
 import React from 'react';
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
